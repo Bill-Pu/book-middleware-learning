9451388ba38749a6b3d89af7b3a27e5864a89e81
PYB <tornado56566@gmail.com>
2025-12-17 17:49:39 +0800

    feature1

---
+++ kafka-middleware/src/main/java/com/learning/kafka/KafkaMiddlewareApplication.java
@@ -0,0 +1,11 @@
+package com.learning.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMiddlewareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaMiddlewareApplication.class, args);
+    }
+}
